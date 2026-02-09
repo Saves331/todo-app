dd47008571cff200c57452a6adb3114462bf0af6
Martin Luterančík <martin.l74123@gmail.com>
2026-02-09 15:30:14 +0100

    starting design of main body in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,14 @@ import Header from '../components/Header'
 
 function Home() {
   return (
-    <div>
+    <section className='bg-cream'>
         <Header />
-    </div>
+        <div className='bg-amber-300 w-[75%] mx-auto py-8'>
+           <h2 className='text-md uppercase font-semibold text-brown'>Your Workspaces</h2>
+        </div>
+
+       
+    </section>
   )
 }
 
