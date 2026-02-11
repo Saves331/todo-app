da6c2ca565560cd3f9363659da991addafd1571a
Saves <martin.l74123@gmail.com>
2026-02-11 14:19:32 +0100

    boardStash has been added

---
+++ src/components/BoardStash.tsx
@@ -0,0 +1,13 @@
+type Props = {
+    skills: string[]
+}
+
+function BoardStash({ skills }: Props) {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default BoardStash
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,21 @@
 import Header from '../components/Header'
+import { FaUser } from "react-icons/fa";
 
 function Home() {
   return (
-    <section className='bg-cream'>
+    <section className='bg-cream h-full'>
         <Header />
-        <div className='bg-amber-300 w-[75%] mx-auto py-8'>
-           <h2 className='text-md uppercase font-semibold text-brown'>Your Workspaces</h2>
+        <div className='w-[75%] mx-auto'>
+          <div className='bg-amber-300 py-8 border-b-2 border-b-orange'>
+           <h2 className='text-lg text-center uppercase font-semibold text-brown'>Your Workspace</h2>
         </div>
 
-       
+        <div className='flex items-center py-5 gap-2'>
+          <FaUser className="text-brown"/>
+            <h2 className='text-md uppercase font-bold text-brown'>Your Projects</h2>
+        </div>
+        </div>
+        
     </section>
   )
 }
